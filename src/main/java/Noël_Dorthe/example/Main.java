88b035491e5ee74b24650400777f63d4e382f4e9
MiaMarta <mia.martaherta@gmail.com>
2022-04-02 15:06:04 +0300

    alustad Boti , module ei ole õigesti path tehtud

--- src/main/java/Noël_Dorthe/example/Main.java
+++ src/main/java/Noël_Dorthe/example/Main.java
@@ -1,5 +1,17 @@
 package Noël_Dorthe.example;
 
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
 public class Main  {
-  //public static JDA jda;
+  public static JDA jda;
+
+  //Main method
+  public static void main(String[] args) throws LoginException {
+    //ehitab uue Boti -- Token tuleb ise lisada
+    //jda = new JDABuilder(AccountType.BOT).setToken("").buildAsync();
+  }
 }
