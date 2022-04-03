3a2fd5189fbd136e43b0ff1c30510c847ce5f6a8
MiaMarta <mia.martaherta@gmail.com>
2022-04-03 13:15:21 +0300

    ei leia enidslet module packi osa JDABuilder

--- src/main/java/org/example/Noel.java
+++ src/main/java/org/example/Noel.java
@@ -1,21 +1,17 @@
-package Noël_Dorthe.example;
+package org.example;
 
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.OnlineStatus;
 
 import javax.security.auth.login.LoginException;
 
-public class Main  {
+public class Noel {
   public static JDA jda;
 
   //Main method
   public static void main(String[] args) throws LoginException {
     //ehitab uue Boti -- Token tuleb ise lisada
     jda = new JDABuilder(AccountType.BOT).setToken("").buildAsync();
-
-  jda.getPresence().setStatus(OnlineStatus.IDLE);
-  //jda.getPresence().setGame(Game.watching("hi!"));
   }
 }
