b18470efce011c5ae3fc47efd265479ef96f7c6e
MiaMarta <mia.martaherta@gmail.com>
2022-04-06 18:18:14 +0300

    õige mavei pox fail ja õige lisa JDAkaustad

--- src/main/java/Noël_Dorthe/example/Main.java
+++ src/main/java/Noël_Dorthe/example/Main.java
@@ -1,9 +1,11 @@
 package Noël_Dorthe.example;
 
+
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
+
 
 import javax.security.auth.login.LoginException;
 
