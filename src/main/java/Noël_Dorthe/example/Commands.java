57d7806549d244ee462e007e86167f6f5f413a4d
MiaMarta <mia.martaherta@gmail.com>
2022-04-12 13:30:40 +0300

    peaks saatma -info peale sisu

---
+++ src/main/java/Noël_Dorthe/example/Commands.java
@@ -0,0 +1,19 @@
+package Noël_Dorthe.example;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.entities.Message;
+
+
+public class Commands extends ListenerAdapter {
+
+    //igakord kui keegi serverisse kirjutab siis see klass registeerib selle
+   public void onGuildMessageReceived(MessageReceivedEvent event){
+   String[] args = event.getMessage().getContentRaw().split(" ");
+
+    if(args[0].equalsIgnoreCase(Main.prefix +"info")){
+        event.getChannel().sendTyping().queue();
+        event.getChannel().sendMessage("Hi, I'm alive").queue();
+    }
+   }
+}
--- src/main/java/Noël_Dorthe/example/Main.java
+++ src/main/java/Noël_Dorthe/example/Main.java
@@ -5,20 +5,26 @@ import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
 
 
 import javax.security.auth.login.LoginException;
 
 public class Main  {
   public static JDA jda;
-
+  public static  String prefix="-";
   //Main method
   public static void main(String[] args) throws LoginException {
     //ehitab uue Boti -- Token tuleb ise lisada
     JDA jda= JDABuilder.createDefault("").build();
 
 
+    //boti staatus on eemal (IDLE)
   jda.getPresence().setStatus(OnlineStatus.IDLE);
-  //jda.getPresence().setGame(Game.watching("hi!"));
+
+  //kuvab, et eemal vaatab telekat, võiv äravõtta
+  jda.getPresence().setActivity(Activity.watching("TV"));
+
+  jda.addEventListener(new Commands());
   }
 }
