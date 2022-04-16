5e5c4eb2d55ad368bfdaeb84c2615d482d4eff9f
ottericottender <otteric.ottender@gmail.com>
2022-04-16 10:34:05 +0300

    Nimetasin enda boti ja sellega seotud klassi ümber "GuessMeRadio" ja lisasin muusika lisamiseks vajalikud dependency'id.

---
+++ src/main/java/Noël_Dorthe/example/GuessMeRadio.java
@@ -0,0 +1,40 @@
+package Noël_Dorthe.example;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class GuessMeRadio extends ListenerAdapter {
+
+    //igakord kui keegi serverisse kirjutab siis see klass registeerib selle
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String[] args = event.getMessage().getContentRaw().split(" ");
+
+        if(args[0].equalsIgnoreCase(Main.prefix+"GuessMeRadio")){
+            event.getChannel().sendTyping().queue();
+            event.getChannel().sendMessage("Hi, GuessMeRadio is alive!").queue();
+            musicGuessing(event);
+        }
+    }
+
+    public void musicGuessing(MessageReceivedEvent event){
+        sleepSek(2);
+        event.getChannel().sendMessage("I will play you a serie of songs and you will have to guess the names of them.").queue();
+        sleepSek(2);
+        event.getChannel().sendMessage("If you want to guess the name of the song be sure to add '?' in front of Your guess.").queue();
+        sleepSek(2);
+        event.getChannel().sendMessage("Here we go!").queue();
+
+    }
+
+    //Meetod, et oleks lihtsam ja kiirem rakendada "sleep" funktiooni sekundites.
+    public static void sleepSek(int n){
+        try {
+            TimeUnit.SECONDS.sleep(n);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/Noël_Dorthe/example/Main.java
+++ src/main/java/Noël_Dorthe/example/Main.java
@@ -1,5 +1,7 @@
 package Noël_Dorthe.example;
 
+
+import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
@@ -9,15 +11,21 @@ import net.dv8tion.jda.api.entities.Activity;
 import javax.security.auth.login.LoginException;
 
 public class Main  {
+  public static JDA jda;
+  public static  String prefix="-";
+  //Main method
   public static void main(String[] args) throws LoginException {
     //ehitab uue Boti -- Token tuleb ise lisada
-      JDA jda= JDABuilder.createDefault("token").build();
+        JDA jda= JDABuilder.createDefault("").build();
+    //
+    //
+    //    //boti staatus on eemal (IDLE)
+      jda.getPresence().setStatus(OnlineStatus.ONLINE);
+    //
+    //  //kuvab, et eemal vatab telekat, võiv äravõtta
+  jda.getPresence().setActivity(Activity.watching("TV"));
 
-      jda.getPresence().setStatus(OnlineStatus.IDLE);
-      jda.getPresence().setActivity(Activity.watching("TV"));
-      jda.addEventListener(new Commands());
-      jda.addEventListener(new MaifBott());
-      jda.addEventListener(new MiaBot());
-      jda.addEventListener(new OttBot());
+  jda.addEventListener(new Commands());
+  jda.addEventListener(new GuessMeRadio());
   }
 }
