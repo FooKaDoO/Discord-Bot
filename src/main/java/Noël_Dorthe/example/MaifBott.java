68da7e96ea21febc673da581ddcd6e8d099d8925
FooKaDoO <92516727+FooKaDoO@users.noreply.github.com>
2022-04-21 15:00:18 +0300

    Maif muutmine ja org.example delete

--- src/main/java/Noël_Dorthe/example/MaifBott.java
+++ src/main/java/Noël_Dorthe/example/MaifBott.java
@@ -4,17 +4,19 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class MaifBott extends ListenerAdapter {
-    public final String prefix = "-";
+    public final String prefix = "maif";
 
     //igakord kui keegi serverisse kirjutab siis see klass registeerib selle
     public void onMessageReceived(MessageReceivedEvent event) {
         String e = event.getMessage().getContentRaw();
         String[] args = e.split(" ");
-        if (args[0].equalsIgnoreCase(prefix + "maif")) {
-            event.getChannel().sendMessage("Olen maifi bot").queue();
-        }
-        if (args[0].equalsIgnoreCase(prefix + "delete")) {
-            event.getChannel().deleteMessageById(event.getMessageId()).queue();
+        if (args[0].equalsIgnoreCase(prefix)) {
+            if(args[1].equalsIgnoreCase("kes")) {
+                event.getChannel().sendMessage("Olen maifi bot").queue();
+            }
+            else if (args[1].equalsIgnoreCase(prefix + "delete")) {
+                event.getChannel().deleteMessageById(event.getMessageId()).queue();
+            }
         }
     }
 }
