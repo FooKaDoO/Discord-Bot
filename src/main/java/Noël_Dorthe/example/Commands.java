a6cec9a0e1836de1c13eed785311f692cd83f2c1
FooKaDoO <92516727+FooKaDoO@users.noreply.github.com>
2022-04-26 13:23:17 +0300

    Small fix

--- src/main/java/Noël_Dorthe/example/Commands.java
+++ src/main/java/Noël_Dorthe/example/Commands.java
@@ -3,12 +3,13 @@ package Noël_Dorthe.example;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.entities.Message;
+import org.jetbrains.annotations.NotNull;
 
 
 public class Commands extends ListenerAdapter {
 
-    //igakord kui keegi serverisse kirjutab siis see klass registeerib selle
-   public void onMessageReceived(MessageReceivedEvent event){
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event){
    String[] args = event.getMessage().getContentRaw().split(" ");
 
     if(args[0].equalsIgnoreCase("-" +"info")){
