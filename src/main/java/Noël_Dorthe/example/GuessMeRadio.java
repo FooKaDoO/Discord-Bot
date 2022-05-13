f31aac458030498a7fe48b16e7acfaed50fdc415
ottericottender <otteric.ottender@gmail.com>
2022-05-14 00:51:14 +0300

    GuessMeRadio ühineb voicechanneliga ja mängib muusikat!

--- src/main/java/Noël_Dorthe/example/GuessMeRadio.java
+++ src/main/java/Noël_Dorthe/example/GuessMeRadio.java
@@ -2,9 +2,10 @@ package Noël_Dorthe.example;
 
 import Noël_Dorthe.example.GuessMeRadioFiles.PlayerManager;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -27,7 +28,30 @@ public class GuessMeRadio extends ListenerAdapter {
         if(args[0].equalsIgnoreCase(prefix+"GuessMeRadio")){
             event.getChannel().sendTyping().queue();
             event.getChannel().sendMessage("Hi, GuessMeRadio is alive!").queue();
-            musicGuessing(event);
+            event.getGuildChannel();
+            Guild guild = event.getGuild();
+            MessageChannel channel=event.getChannel();
+            Member self = guild.getSelfMember();
+            GuildVoiceState selfVoiceState = self.getVoiceState();
+            Member member = event.getMember();
+            GuildVoiceState memberVoiceState = member.getVoiceState();
+
+
+
+            if(selfVoiceState.inAudioChannel()){
+                channel.sendMessage("I'm already in a voice channel!").queue();
+            }
+            else if (!memberVoiceState.inAudioChannel()){
+                channel.sendMessage("You are not in a voice channel!").queue();
+            }
+            else{
+                AudioManager audioManager = guild.getAudioManager();
+                AudioChannel memberChannel = memberVoiceState.getChannel();
+                audioManager.openAudioConnection(memberChannel);
+                channel.sendMessage("Connecting to: " + memberChannel.getName()).queue();
+                musicGuessing(event);
+
+            }
         }
     }
 
